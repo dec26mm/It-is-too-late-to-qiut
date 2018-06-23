ed07d92a85e141e89425b6d71ac4ffbe38977d66
dec26mm <ma8sa0ko4>
2018-06-23 16:07:03 +0800

    make data together

---
+++ data pretreatment.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+with open('data/users.csv', encoding='utf-8') as f:
+    User = pd.read_csv(f)
+
+with open('data/implicit_ratings.csv', encoding='utf-8') as f:
+    ZeroRating = pd.read_csv(f)
+
+with open('data/book_ratings_train.csv', encoding='utf-8') as f:
+    Train = pd.read_csv(f)
+
+User['Age'] = User['Age'].fillna(User['Age'].mean())
+Train = pd.concat([Train, ZeroRating], keys=['User-ID', 'ISBN', 'Book-Rating'], axis=0)
+
+Data = pd.merge(Train, User, on=['User-ID'], how='outer')
+Data = Data.fillna(0)
+
+Data.to_csv('user data.csv', encoding='utf-8')
+
+
+
+
