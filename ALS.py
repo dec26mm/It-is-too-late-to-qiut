69074de433763fc0e7b96a9ace4259b98dde6c1e
dec26mm <ma8sa0ko4>
2018-06-24 16:51:12 +0800

    If book or user didn't appear before, the prediction would be a random integer.

--- ALS.py
+++ ALS.py
@@ -2,6 +2,7 @@ import pandas as pd
 import implicit
 import numpy as np
 from scipy.sparse import coo_matrix
+import random
 
 with open('user data.csv', encoding='utf-8') as f:
     Data = pd.read_csv(f)
@@ -18,7 +19,7 @@ Rating = coo_matrix((np.ones(Data.shape[0]),
                     (Data['ISBN'].cat.codes.copy(),
                     Data['User-ID'].cat.codes.copy())))
 # train model
-Model = implicit.als.AlternatingLeastSquares(factors=50, regularization=0.01, dtype=np.float64, iterations=50)
+Model = implicit.als.AlternatingLeastSquares(factors=50, regularization=5000, dtype=np.float64, iterations=50)
 Confidence = 4000
 Model.fit(Confidence * Rating)
 
@@ -29,28 +30,35 @@ User = dict(enumerate(Data['User-ID'].cat.categories))
 UserId = {r: i for i, r in User.items()}
 
 Predictions = []
+j = 0
 
 for i, k in zip(Test['User-ID'], Test['ISBN']):
     try:
         UserVec = Model.user_factors[UserId['%s' % i]]
-        BookVec = Model.item_factors[ISBNId['%s' % k]]
-        PredictValue = np.dot(BookVec, UserVec.T)
+        # BookVec = Model.item_factors[ISBNId['%s' % k]]
+        PredictVec = np.dot(UserVec, Model.item_factors.T)
+        PredictVec = PredictVec * (10/PredictVec.max())  # normalize
+        PredictValue = PredictVec[ISBNId['%s' % k]]  # find the exact value
         if PredictValue >= 10:
             PredictValue = 10
         if PredictValue < 1:
-                PredictValue = 1
+            PredictValue = 1
         Predictions.append(PredictValue)
     except KeyError:
-        Predictions.append(5)  # the average of 1-10
+        Predictions.append(random.randint(1, 10))
     except IndexError:
-        Predictions.append(5)  # the average of 1-10
+        Predictions.append(random.randint(1, 10))
+    j = j + 1
 
-    print(i)
-    print(k)
-    print(Predictions[-1])
+    # print(i)
+    # print(k)
+    print('round: %s , prediction: %s' % (j, Predictions[-1]))
 
 Output = pd.DataFrame(Predictions, columns=['Rating'])
+Output_ = pd.DataFrame([None]*len(Predictions), columns=['Rating'])
 
-Output.to_csv('try 2.csv', index=False, header=False)
+# Output['Rating'] = Output['Rating'].fillna(Output['Rating'].mean())
+Output.to_csv('try4_float.csv', index=False, header=False)
 
-
+Output_['Rating'] = Output['Rating'].astype('int64')
+Output_.to_csv('try4_int.csv', index=False, header=False)
